bc4fc5b6f4f332e434c5cb702d85152851deaf32
tsssni <dingyongyu2002@foxmail.com>
2023-11-23 19:47:14 +0800

    remove auto installation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,25 +8,11 @@ return {
     require("mason").setup({})
     require("mason-lspconfig").setup({})
     
-    local registry = require("mason-registry")
-    local function check_installation(...)
-      if not registry.is_installed(...) then
-        for _, v in ipairs{...} do
-          vim.cmd(":MasonInstall "..v)
-        end
-      end
-    end
-    check_installation(
-      "lua-language-server",
-      "clangd",
-      "cmake-language-server",
-      "codelldb"
-    )
-
     local lsp_config = require("lspconfig")
     lsp_config.clangd.setup({})
     lsp_config.cmake.setup({})
     lsp_config.lua_ls.setup({})
+
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
     vim.keymap.set("n", "gR", vim.lsp.buf.rename, {})
