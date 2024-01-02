ac9bc954120b4c5f9ee00a5ca89c41a04346df2d
tsssni <dingyongyu2002@foxmail.com>
2024-01-02 20:15:06 +0800

    fix mason icons

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,7 +5,15 @@ return {
     'williamboman/mason-lspconfig.nvim',
   },
   config = function()
-    require'mason'.setup{}
+    require'mason'.setup{
+      ui = {
+        icons = {
+          package_installed = '',
+          package_pending = '',
+          package_uninstalled = '',
+        }
+      }
+    }
     require'mason-lspconfig'.setup{}
 
     local lsp_config = require'lspconfig'
